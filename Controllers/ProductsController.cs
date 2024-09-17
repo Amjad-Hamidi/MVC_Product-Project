cbaaee9b0a291bfdf8d6622219d6cdafb5ca8c3c
Amjad-Hamidi <Amjad-Hamidi>
2024-09-17 19:18:48 +0300

    MVC-Product

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using Microsoft.AspNetCore.Mvc;
 using ProductApp.Data;
 using ProductApp.Models;
@@ -81,3 +82,95 @@ namespace ProductApp.Controllers
         }
     }
 }
+=======
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductApp.Data;
+using ProductApp.Models;
+
+namespace ProductApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Products.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product model) 
+        { 
+            _context.Products.Add(model);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult changeProductPublish(int id)
+        {
+            var product= _context.Products.Find(id);
+            if (product.InStock && product.Qty!=0) { 
+                product.InPublish = !product.InPublish;
+            }
+            else
+            {
+                product.InPublish = false;
+            }
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult changeProductAvailability(int id)
+        {
+            var product = _context.Products.Find(id);
+            if(product.Qty != 0)
+            { 
+			    product.InStock = !product.InStock;
+            }
+            else
+            {
+                product.InStock = false;
+            }
+            if (!product.InStock)
+			{
+				product.InPublish = false;
+				product.Qty = 0;
+			}
+			_context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult Edit(int id)
+        {
+            return View(_context.Products.Find(id)); // id صاحب هاد ال  products برجع 
+        }
+        [HttpPost]
+        public IActionResult Edit(Product prod) 
+        {
+            var product = _context.Products.Find(prod.ProductId);
+            product.Name = prod.Name;
+            product.Price = prod.Price;
+            product.Qty = prod.Qty;
+
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        
+        public IActionResult Delete(int id) 
+        {
+            var product = _context.Products.Find(id);
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
+>>>>>>> ef08756 (MVC-Product)
--- obj/Debug/net8.0/ProductApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProductApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d07113f779c125c193b15800aa4b43801148c4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
